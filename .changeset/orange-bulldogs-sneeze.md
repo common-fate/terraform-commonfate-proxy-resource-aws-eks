fa7b7da6ff3c7fd4609d04491dd08090d70a72ee
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-11-04 15:01:27 +0800

    changeset initial release (#1)
    * changeset initial release
    
    * comment out resources
    
    * rework roles module
    
    * split module
    
    * remove variable

---
+++ .changeset/orange-bulldogs-sneeze.md
@@ -0,0 +1,5 @@
+---
+"@common-fate/terraform-commonfate-proxy-resource-aws-eks": minor
+---
+
+Initial release configures the AWS EKS proxy integration by creating a cluster resource and optionally deploying read only and admin roles into the cluster
--- variables.tf
+++ variables.tf
@@ -36,12 +36,3 @@ variable "name" {
   type        = string
   default     = ""
 }
-
-
-
-
-variable "create_default_rbac_roles" {
-  description = "If 'true', will create default admin and read_only k8s rbac roles that allow the default Common Fate impersonate user roles to access the cluster."
-  type        = bool
-  default     = false
-}
